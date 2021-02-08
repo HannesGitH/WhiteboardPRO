7296a76e10406b17ee6246a45457d6c5826d51d6
hannes <hannes@gfzdfuz.de>
2021-02-08 12:41:59 +0100

    now with DarkMode

--- Chrome-Extension/WhiteboardPRO/background.js
+++ Chrome-Extension/WhiteboardPRO/background.js
@@ -2,5 +2,5 @@ let installed = 1;
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ installed });
-  chrome.storage.sync.set({ color:'#333333' });
+  chrome.storage.sync.set({ color:'#e4e4e5' });
 });
